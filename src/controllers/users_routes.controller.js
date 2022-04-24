d62bee80095e3b105f260611a240990b504fdd02
Jaime Sierra <49314853+jhadechni@users.noreply.github.com>
2022-04-24 15:40:34 -0500

    feat: posts endpoints finished pending for test

--- src/controllers/users_routes.controller.js
+++ src/controllers/users_routes.controller.js
@@ -1,28 +1,28 @@
-const controller = {};
-const userModel = require('../models/userModel.ts');
+const controller = {}
+const userModel = require('../models/userModel.js')
 
 //get User
 controller.getUser = async (req, res) => {
-    console.log("enter in this endpoint")
     try {
         const user = await userModel.findById(req.query.user_id)
         if (!user) {
             res.status(404).json({ data: "User not found" })
         } else {
-            res.status(200).json({ data: user })
+            res.status(200).json(user)
         }
     } catch (error) {
         console.log(error)
         res.status(500).json({ data: "Server internal error" })
     }
 }
+
 //Register
 controller.register = async (req, res) => {
     const user = await userModel.findOne({ username: req.body.username })
     try {
         if (!user) {
             const newUser = await userModel.create(req.body)
-            res.status(201).json({ data: newUser })
+            res.status(201).json(newUser)
         } else {
             res.status(208).json({ data: "User already exist" })
         }
@@ -30,8 +30,6 @@ controller.register = async (req, res) => {
         console.log(error)
         res.status(500).json({ data: "Server internal error" })
     }
-
-
 }
 
 //Login
@@ -41,7 +39,7 @@ controller.login = async (req, res) => {
         if (!user) {
             res.status(404).json({ data: "Username or password incorrect" })
         } else {
-            res.status(200).json({ data: user })
+            res.status(200).json(user)
         }
 
     } catch (error) {
@@ -57,8 +55,7 @@ controller.prevLogin = async (req, res) => {
         if (!user) {
             res.status(404).json({ data: "User not found" })
         } else {
-            console.log("prevlogin",user)
-            res.status(200).json({ data: user })
+            res.status(200).json(user)
         }
 
     } catch (error) {
--- src/routes/routesConfig.js
+++ src/routes/routesConfig.js
@@ -1,7 +1,9 @@
 const userRouters = require('./user_routes')
+const postRouters = require('./post_routes')
 
 const routes = (app) => {
     app.use("/users", userRouters)
+    app.use("/posts", postRouters)
 };
 
 
