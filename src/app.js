27f7e2e787041c8dd232b0cc4aaeed4136ed9d6e
Jaime Sierra <49314853+jhadechni@users.noreply.github.com>
2022-04-26 13:29:20 -0500

    fixing cors 2

--- src/app.js
+++ src/app.js
@@ -13,16 +13,8 @@ app.use(express.json());
 app.use(morgan('tiny'))
 
 //coors settings
-app.use(( req, res, next ) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    if ( req.method === 'OPTIONS' ) {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return res.status(200).json({});
-    }
-    next();
-   });
-   
+app.use(cors());
+
 //routes
 router(app);
 
