18c17271f010fb385fc0131ccc5ad63dee14d077
Jaime Sierra <49314853+jhadechni@users.noreply.github.com>
2022-04-26 13:21:54 -0500

    fixing coors

--- src/app.js
+++ src/app.js
@@ -11,6 +11,18 @@ app.set('port',process.env.PORT || 4000);
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'))
+
+//coors settings
+app.use(( req, res, next ) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    if ( req.method === 'OPTIONS' ) {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+    }
+    next();
+   });
+   
 //routes
 router(app);
 
