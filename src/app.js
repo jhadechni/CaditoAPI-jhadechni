6d735230499f00e493450103c49f7dfa05fdd690
Jaime Sierra <49314853+jhadechni@users.noreply.github.com>
2022-04-26 22:10:22 -0500

    fixed cors??

--- src/app.js
+++ src/app.js
@@ -1,29 +1,19 @@
-const express = require ('express');
+const express = require('express');
 const cors = require('cors');
 const router = require('./routes/routesConfig');
 const app = express();
 const morgan = require('morgan');
 
 //settings
-app.set('port',process.env.PORT || 4000);
+app.set('port', process.env.PORT || 4000);
 
 //middlewares
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan('tiny'))
-
-//coors settings
-app.use(( req, res, next ) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    if ( req.method === 'OPTIONS' ) {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return res.status(200).json({});
-    }
-    next();
-   });
 
 //routes
 router(app);
 
-module.exports = app ;
+module.exports = app;
